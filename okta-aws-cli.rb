83335101e71579918fed0f923f2bc86426519f0e
Tien Nguyen <duytien.nguyen@okta.com>
2023-02-10 14:26:28 -0500

    update

--- okta-aws-cli.rb
+++ okta-aws-cli.rb
@@ -14,15 +14,5 @@ class OktaAwsCli < Formula
     str_help = shell_output("#{bin}/okta-aws-cli --help")
     assert_match "Usage::", str_help
     assert_match "Flags:", str_help
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test okta-aws-cli`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    # system "false"
   end
 end
